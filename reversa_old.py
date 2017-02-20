172b532ce85ea95f11492d8832b9bc82db681131
Daniela Sanchez <dani.sanchez.96@hotmail.com>
2017-02-20 11:35:04 -0500

    add check module

---
+++ reversa_old.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import pandas as pd
+
+from preprocessing import Preprocessing
+from similarity import Similarity
+from composition import Composition
+from pca import Reduction
+from clustering import Clustering
+from cluster import ClusterReport
+from postprocessing import Validate
+from jplace import ParseJplace
+from stats import Profiles
+from binomial import StatsBinom
+from clean import cleaning
+from parse import arguments
+
+
+def main(args, config):
+    wDir = os.getcwd()
+    #Instance Preprocessing class
+    window = Preprocessing(args.fasta_file, config['win_length'], config['win_step'])
+    window.output_window()
+    print >> sys.stderr, "Creating windows_sequence.fasta"
+    
+    #Instance Similarity and Composition class
+    sim = Similarity(args.fasta_file, config['score_adj'],wDir)
+    sim_matrix = sim.mcl_perform() 
+    comp_results = Composition(config['kmer_len'])
+    comp_matrix = comp_results.joined()
+    #Join similarity and composition matrix for PCA
+    join = pd.concat([comp_matrix, sim_matrix], axis= 1, join='inner')
+    print >> sys.stderr, "Calculating similarity and composition matrix"
+    
+    #Instance Reduction class
+    pca = Reduction(join, config['pca_comp'])
+    pca_data = pca.perform_pca()
+    print >> sys.stderr, "Performing PCA"
+    
+    #Instance Clustering class
+    cluster = Clustering(pca_data)
+    clust_obj = cluster.plot()
+    print >> sys.stderr, "Performing clustering plot"
+    
+    #Instance ClusterReport class
+    report = ClusterReport(clust_obj)
+    file_name, querySeq = report.output_queryseq()
+    print >> sys.stderr, "Doing report of clusters"
+
+    #Instance Validate class
+    valid = Validate(file_name, args.fasta_file,wDir)
+    jfileComp, jfileMinus = valid.roundTwo()
+    print >> sys.stderr, "Validation of results"
+    
+    #Instance ParseJplace Class
+    parsing = ParseJplace(jfileComp, jfileMinus)
+    corrMat = parsing.correlation()
+    print >> sys.stderr, "Doing profiles"
+    
+    #Instance Profile Class
+    ttest = Profiles(corrMat, querySeq)
+    bestWin = ttest.windowsAssigment()
+    print >>sys.stderr, "Doing permutations"
+    
+    #Instance StatsBinom
+    finalResult = StatsBinom(args.fasta_file, config['win_length'],bestWin)
+    finalResult.binomial()
+    
+    cleaning(file_name)
+
+
+if __name__ == '__main__':
+    args, config = arguments()
+    results = main(args, config)
+    print >> sys.stderr, "ReverSa Finished"
